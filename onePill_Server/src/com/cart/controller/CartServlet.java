d170b2be11da1b4da6bace2fa6954655724815f8
zhanghaowei123 <2904845562@qq.com>
2019-12-18 08:08:43 +0800

    购物车 张昊伟

---
+++ onePill_Server/src/com/cart/controller/CartServlet.java
@@ -0,0 +1,51 @@
+package com.cart.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.cart.dao.cartsDao;
+import com.entity.Medicine;
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class CartServlet
+ */
+@WebServlet("/CartServlet")
+public class CartServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CartServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		int medicineId = Integer.parseInt(request.getParameter("medicineId"));
+		List<Medicine> medicine = new cartsDao().findAllMedicineInfo(medicineId);
+	
+		response.getWriter().append(new Gson().toJson(medicine));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
